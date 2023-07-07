e377b50c781681c14044392c1a77679dbd0e75c3
FlaringK <caelans@iinet.net.au>
2023-07-07 20:53:43 +0800

    Ticker and Music Player Keep

--- sahcon.js
+++ sahcon.js
@@ -10,4 +10,13 @@ const loadPage = async () => {
   document.getElementById("content").innerHTML = pageHtml
 }
 
+const clickLink = (event, link) => {
+  console.log(link)
+  event.preventDefault()
+  history.pushState(null, '', link)
+  loadPage()
+}
+
+document.querySelectorAll(".pageLink").forEach(a => { a.addEventListener("click", evt => clickLink(evt, a.href)) })
+
 loadPage()
